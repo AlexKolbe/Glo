243dc736272ac616d14e52cdce062b57cab9dfa3
AlexKolbe <helping5@yandex.ru>
2026-02-06 13:48:08 +0300

    lesson06-hard домашка

--- game.js
+++ game.js
@@ -1,15 +1,36 @@
-function createGuessingGame() {
-	// Замыкание для хранения загаданного числа
-	const secretNumber = Math.floor(Math.random() * 100) + 1;
+function createGameBot() {
+	// Замыкание для хранения состояния игры
+	let secretNumber;
+	let attemptsLeft;
 
-	console.log('Загадано число от 1 до 100. Попробуйте угадать!');
+	function initGame() {
+		secretNumber = Math.floor(Math.random() * 100) + 1;
+		attemptsLeft = 10;
+		console.log('Новая игра! Угадай число от 1 до 100');
+		console.log(`У вас есть ${attemptsLeft} попыток.`);
+		console.log('-----------------------------------');
+		askGuess();
+	}
 
 	function askGuess() {
-		const userInput = prompt('Угадай число от 1 до 100');
+		// Если попытки закончились
+		if (attemptsLeft <= 0) {
+			console.log(`Попытки закончились. Загаданное число было: ${secretNumber}`);
+			const playAgain = confirm('Попытки закончились, хотите сыграть еще?');
+			if (playAgain) {
+				initGame();
+			} else {
+				console.log('До свидания! Спасибо за игру!');
+			}
+			return;
+		}
+
+		const userInput = prompt(`Попытка ${11 - attemptsLeft}. Введите число от 1 до 100 (осталось попыток: ${attemptsLeft}):`);
 
 		// Если пользователь нажал "Отмена"
 		if (userInput === null) {
-			console.log('Игра окончена');
+			console.log('Игра окончена. До свидания!');
+			console.log(`Загаданное число было: ${secretNumber}`);
 			return;
 		}
 
@@ -19,24 +40,36 @@ function createGuessingGame() {
 		// Проверяем, является ли ввод числом
 		if (isNaN(userNumber) || userInput.trim() === '') {
 			console.log('Введи число!');
-			// Рекурсивно вызываем функцию снова
+			console.log(`Осталось попыток: ${attemptsLeft}`);
+			// Рекурсивно вызываем функцию снова (попытка не тратится)
 			return askGuess();
 		}
 
 		// Проверяем, что число в диапазоне
 		if (userNumber < 1 || userNumber > 100) {
 			console.log('Введи число от 1 до 100!');
+			console.log(`Осталось попыток: ${attemptsLeft}`);
+			// Попытка не тратится на некорректный ввод
 			return askGuess();
 		}
 
+		// Уменьшаем количество попыток при корректном вводе
+		attemptsLeft--;
+
 		// Сравниваем числа
 		if (userNumber === secretNumber) {
-			console.log('Поздравляю, Вы угадали!!!');
+			console.log(`Поздравляю, Вы угадали число ${secretNumber}!`);
+			const playAgain = confirm('Хотели бы сыграть еще?');
+			if (playAgain) {
+				initGame();
+			} else {
+				console.log('Спасибо за игру! До новых встреч!');
+			}
 			return;
 		} else if (userNumber > secretNumber) {
-			console.log('Загаданное число меньше');
+			console.log(`Загаданное число меньше ${userNumber}, осталось попыток: ${attemptsLeft}`);
 		} else {
-			console.log('Загаданное число больше');
+			console.log(`Загаданное число больше ${userNumber}, осталось попыток: ${attemptsLeft}`);
 		}
 
 		// Рекурсивно вызываем функцию для следующей попытки
@@ -44,9 +77,9 @@ function createGuessingGame() {
 	}
 
 	// Возвращаем функцию для начала игры
-	return askGuess;
+	return initGame;
 }
 
 // Запуск игры
-const startGame = createGuessingGame();
+const startGame = createGameBot();
 startGame();
